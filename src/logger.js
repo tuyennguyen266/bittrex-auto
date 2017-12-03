0037794382a7807672bdf54895391018cd91a60d
Tuyen Nguyen <tuyennguyen2015@gmail.com>
2017-12-03 16:14:14 +0700

    feat: remove log in console

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,6 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console(),
     new winston.transports.File({ filename: 'combined.log' }),
     new winston.transports.File({ filename: 'error.log', level: 'error' })
   ]
