e1df03aa8f4f522291665b212bfa45b4e9be1b77
Tuyen Nguyen <tuyennguyen2015@gmail.com>
2017-12-04 21:38:21 +0700

    feat: upgrade logger format, add timestamp

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,20 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+const config = require('./config');
 
-const logger = winston.createLogger({
+const myFormat = printf(info => {
+  return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    label({ label: config.market }),
+    timestamp(),
+    myFormat
+  ),
   transports: [
-    new winston.transports.File({ filename: 'combined.log' }),
-    new winston.transports.File({ filename: 'error.log', level: 'error' })
+    new transports.File({ filename: 'combined.log' }),
+    new transports.File({ filename: 'error.log', level: 'error' })
   ]
 });
 
